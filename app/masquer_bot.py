163fb70612feea27d3009d4f662b85fb81cc8379
ra101 <ra101@protonmail.com>
2020-07-28 12:50:08 +0530

    /get_key

--- app/masquer_bot.py
+++ app/masquer_bot.py
@@ -1,4 +1,5 @@
 import os
+from hashlib import sha512
 
 import telebot
 from dotenv import load_dotenv
@@ -20,12 +21,31 @@ def start(message):
     username = message.from_user.username
     account = UserAccount(username)
     db.session.add(account)
-    db.session.commit()
-    bot.reply_to(message, account.public_key)
+    try:
+        db.session.commit()
+        bot.send_message(message.chat.id, account.public_key)
+    except:
+        pass
+
+
+@bot.message_handler(commands=["get_key"])
+def get_key(message):
+    username = message.from_user.username
+    try:
+        bot.send_message(
+            message.chat.id,
+            UserAccount.query.filter_by(
+                username_hash=sha512(bytes(username, "utf-8")).hexdigest()
+            )
+            .first()
+            .public_key,
+        )
+    except:
+        bot.reply_to(message, "Unable to fetch userkey.")
 
 
 @bot.message_handler(commands=["help"])
-def start(message):
+def help(message):
     bot.reply_to(message, "Yo! help arrived!")
 
 
