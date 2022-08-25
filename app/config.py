2f972e568ca485826e2165d820d11b75696eace9
Parth Agarwal <agarwal.parth.101@protonmail.com>
2022-08-26 00:13:11 +0530

    Code Update

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
-'''
+"""
 ./app/config.py
-'''
+"""
 import os
 
 from dotenv import load_dotenv
@@ -12,7 +12,7 @@ DEBUG = True if os.getenv("FLASK_DEBUG") == "True" else False
 SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 ENV = os.getenv("FLASK_ENV")
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.getenv("BOREALIS_PG_PINK_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = (
     True if os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS") == "True" else False
 )
